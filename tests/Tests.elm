d09337e895d1dec39796f326b9328d247921d508
Rémy HUBSCHER <rhubscher@mozilla.com>
2016-09-20 21:23:34 +0200

    Add a first test.

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,14 +1,27 @@
 module Tests exposing (..)
 
+import Html
+import Html.Attributes
 import Test exposing (..)
 import Expect
 import String
+import Html
+import App exposing (displayImage, attachmentUrl)
 
 
 all : Test
 all =
-    describe "A Test Suite"
-        [ test "Addition" <|
+    describe "Test display Image"
+        [ test "Image should display the Author" <|
             \() ->
-                Expect.equal (3 + 7) 10
+                Expect.equal (displayImage { title = "Rémy", url = "file.jpg" })
+                    (Html.li
+                        []
+                        [ Html.img
+                            [ Html.Attributes.src (attachmentUrl ++ "file.jpg")
+                            , Html.Attributes.title "Author: Rémy"
+                            ]
+                            []
+                        ]
+                    )
         ]
